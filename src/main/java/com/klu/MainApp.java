98f157e3bbca944a81b8baa963b00d65c36b925b
kl2400031807 <2400031807@kluniversity.in>
2026-01-02 08:36:49 +0530

    Bug fix applied

--- src/main/java/com/klu/MainApp.java
+++ src/main/java/com/klu/MainApp.java
@@ -2,8 +2,8 @@ package com.klu;
 
 public class MainApp {
  public static void main (String[] args){
-	 System.out.println(" Welcome to git");
-	 System.out.println(" Git version control");
+	 System.out.println(" Bug fix");
+
 	 
  }
 }
