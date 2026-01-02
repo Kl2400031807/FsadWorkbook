e6376cfcc15653050ffed5a884c9f3a11339dfd3
kl2400031807 <2400031807@kluniversity.in>
2026-01-02 08:32:48 +0530

    02-01-2026

---
+++ src/main/java/com/klu/MainApp.java
@@ -0,0 +1,9 @@
+package com.klu;
+
+public class MainApp {
+ public static void main (String[] args){
+	 System.out.println(" Welcome to git");
+	 System.out.println(" Git version control");
+	 
+ }
+}
